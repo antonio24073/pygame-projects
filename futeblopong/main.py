ccbadfdb846cca0f687f6fbea70bf8b05e555592
Antonio Augusto <antonio.etec.inf5@gmail.com>
2024-09-18 06:51:56 -0300

    Movimento vertical da bola e colisão segundo jogador

--- futeblopong/main.py
+++ futeblopong/main.py
@@ -13,11 +13,13 @@ player1_moveup = False
 player1_movedown = False
 
 player2 = pygame.image.load("assets/player2.png")
+player2_y = 310
 
 ball = pygame.image.load("assets/ball.png")
 ball_x = 617
 ball_y = 337
-ball_dir = -2
+ball_dir = -10
+ball_dir_y = 8
 
 def move_player():
     global player1_y
@@ -38,23 +40,42 @@ def move_player():
     elif player1_y >= 575:
         player1_y = 575
 
+def move_player2():
+    global player2_y
+    player2_y = ball_y # fazendo segundo jogador defender todas
+
 def move_ball():
     global ball_x
     global ball_y
     global ball_dir
+    global ball_dir_y
 
     ball_x += ball_dir
+    ball_y += ball_dir_y
 
     if ball_x < 120:
         if player1_y < ball_y + 23:
             if player1_y + 146 > ball_y:
                 ball_dir *= -1
+    if ball_x < 0:
+        ball_dir *= -1
 
+    if ball_x > 1100:
+        if player2_y < ball_y + 23:
+            if player2_y + 146 > ball_y:
+                ball_dir *= -1
+    if ball_x > 1280:
+        ball_dir *= -1
+
+    if ball_y > 680:
+        ball_dir_y *= -1
+    elif ball_y <= 40:
+        ball_dir_y *= -1
 
 def draw():
     window.blit(field, (0,0))
     window.blit(player1, (50, player1_y))
-    window.blit(player2, (1150, 310))
+    window.blit(player2, (1150, player2_y))
     window.blit(ball, (ball_x, ball_y))
 
 ball = pygame.image.load("assets/ball.png")
@@ -82,6 +103,7 @@ while loop:
     #     loop = False
 
     move_player()
+    move_player2()
     draw()
     move_ball()
     pygame.display.update()
