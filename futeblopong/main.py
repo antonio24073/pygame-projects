3078d7ef7084e51b0088a77263d2e494635fd24f
Antonio Augusto <antonio.etec.inf5@gmail.com>
2024-09-18 06:10:21 -0300

    Melhor movimento do jogador

--- futeblopong/main.py
+++ futeblopong/main.py
@@ -9,6 +9,8 @@ field = pygame.image.load("assets/field.png")
 
 player1 = pygame.image.load("assets/player1.png")
 player1_y = 310
+player1_moveup = False
+player1_movedown = False
 
 player2 = pygame.image.load("assets/player2.png")
 
@@ -16,6 +18,24 @@ ball = pygame.image.load("assets/ball.png")
 ball_x = 617
 ball_y = 337
 
+def move_player():
+    global player1_y
+
+    if player1_moveup:
+        player1_y -= 5
+    else:
+        player1_y -= 0
+
+    if player1_movedown:
+        player1_y += 5
+    else:
+        player1_y -= 0
+
+    # print(player1_y)
+    if player1_y <= 0:
+        player1_y = 0
+    elif player1_y >= 575:
+        player1_y = 575
 
 def move_ball():
     global ball_x
@@ -37,14 +57,16 @@ while loop:
         if events.type == pygame.QUIT:
             loop = False
         if events.type == pygame.KEYDOWN:
-            print("keydown")
             if events.key == pygame.K_w:
-                print("K_w")
-                player1_y -= 10
+                player1_moveup = True
             if events.key == pygame.K_s:
-                player1_y += 10
-
-
+                player1_movedown = True
+        if events.type == pygame.KEYUP:
+            if events.key == pygame.K_w:
+                player1_moveup = False
+            if events.key == pygame.K_s:
+                player1_movedown = False
+    move_player()
     draw()
     move_ball()
     pygame.display.update()
